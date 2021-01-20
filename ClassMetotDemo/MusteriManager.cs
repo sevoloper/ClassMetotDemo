17be5d4bd2d638b728b2578f08bff54475f454d2
sevoloper <76591530+sevoloper@users.noreply.github.com>
2021-01-21 01:30:10 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkleme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " eklendi");
+        }
+
+        public void MusteriSilme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " silindi");
+        }
+
+        public void Listeleme(Musteri[] musteri)
+        {
+            Console.WriteLine("Müşteriler");
+
+            foreach (var item in musteri)
+            {
+                Console.Write(item.Id + " ");
+                Console.Write(item.Ad + " ");
+                Console.Write(item.Soyad);
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri
+            {
+                Id = 0,
+                Ad = "Kaan",
+                Soyad = "Epçeçen"
+            };
+
+            Musteri musteri2 = new Musteri
+            {
+                Id = 1,
+                Ad = "Şevval",
+                Soyad = "Ülkü"
+            };
+
+            Musteri musteri3 = new Musteri
+            {
+                Id = 2,
+                Ad = "Ceren",
+                Soyad = "Epçeçen"
+            };
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkleme(musteri1);
+            musteriManager.MusteriEkleme(musteri2);
+
+            musteriManager.MusteriSilme(musteri3);
+
+            
+
+            musteriManager.Listeleme(musteriler);
+        }
+    }
+}
